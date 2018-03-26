b904a55c50928f9b3a5025e8f573fd8c5ce406c8
mangomilk114 <mangomilk114@gmail.com>
2018-03-26 13:31:10 +0900

    [배환웅] 기본 이미지 불러오는 기능 추가중

--- app/src/main/java/mangomilk/marumaruviewer/MainActivity.java
+++ app/src/main/java/mangomilk/marumaruviewer/MainActivity.java
@@ -1,11 +1,14 @@
 package mangomilk.marumaruviewer;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -14,11 +17,12 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     private String htmlPageUrl = "http://www.daum.net/";
-    private TextView textviewHtmlDocument;
+    private ImageView textviewHtmlDocument;
     private String htmlContentInStringFormat;
 
 
@@ -28,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        textviewHtmlDocument = (TextView)findViewById(R.id.textView);
-        textviewHtmlDocument.setMovementMethod(new ScrollingMovementMethod());
+        textviewHtmlDocument = (ImageView)findViewById(R.id.textView);
+        //textviewHtmlDocument.setMovementMethod(new ScrollingMovementMethod());
 
         Button htmlTitleButton = (Button)findViewById(R.id.button);
         htmlTitleButton.setOnClickListener(new View.OnClickListener() {
@@ -52,13 +56,26 @@ public class MainActivity extends AppCompatActivity {
         protected Void doInBackground(Void... params) {
             try {
 
-                Document doc = Jsoup.connect(htmlPageUrl).get();
-                Elements links = doc.select("a[href]");
+                Document doc = Jsoup.connect("http://wasabisyrup.com/archives/412625").get();
+                Elements links = doc.select("#lz-lazyloaded");
+                String imgSrc = links.attr("src");
+                InputStream input = new java.net.URL("http://wasabisyrup.com/" + imgSrc).openStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(input);
+                textviewHtmlDocument.setImageBitmap(bitmap);
+                /*Document document = Jsoup.connect(url).get();
+                Elements img = document.select("div.col-sm-12.A4DZP4C3.well.leaderboard-A4DZP4C3.genericContainer.js-A4DZP4C3.js-leaderboard-A4DZP4C3");
+                // Locate the src attribute
+                String imgSrc = img.attr("src");
+                // Download image from URL
+                InputStream input = new java.net.URL(imgSrc).openStream();
+                // Decode Bitmap
+                BitmapFactory
+                bitmap = BitmapFactory.decodeStream(input);*/
 
-                for (Element link : links) {
+                /*for (Element link : links) {
                     htmlContentInStringFormat += (link.attr("abs:href")
                             + "("+link.text().trim() + ")\n");
-                }
+                }*/
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -68,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Void result) {
-            textviewHtmlDocument.setText(htmlContentInStringFormat);
+            //textviewHtmlDocument.setText(htmlContentInStringFormat);
         }
     }
 }
